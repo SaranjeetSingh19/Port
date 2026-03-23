9b25695079ddf923c9604624e09a827eb9ab633b
SaranjeetSingh19 <152581941+SaranjeetSingh19@users.noreply.github.com>
2026-03-23 16:38:50 +0530

    Update hero-content.tsx

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -28,7 +28,7 @@ export const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Welcome to S4
+            Welcome to Dev Lance
           </h1>
         </motion.div>
 
